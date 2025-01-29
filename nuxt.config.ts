264de847547e6326311de60cd63252910bd36214
gorkavillara <gorkavillara@gmail.com>
2025-01-29 19:33:28 +0100

    Dashboard con SSG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@pinia/nuxt']
-})
+  css: ["~/assets/css/main.css"],
+  modules: ["@pinia/nuxt"],
+  ssr: true,
+  routeRules: {
+    "/": { prerender: true },
+    "/unauthorized": { prerender: true },
+    "/login": { prerender: true },
+  },
+  // nitro: {
+  //   prerender: {
+  //     routes: ["/", "/unauthorized", "/login"]
+  //   }
+  // }
+});
